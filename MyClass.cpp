e55f9a4f6d87416a8cad5ab82a20711ed01cc0f3
Max <meitstudying@gmail.com>
2024-10-31 21:35:37 +0200

    hotfix

--- MyClass.cpp
+++ MyClass.cpp
@@ -2,5 +2,15 @@
 #include <cmath>
 
 double MyClass::FuncA() {
-    return 0;
+    double sum = 3;
+
+    for (int i = 1; i < 3; ++i) {
+        if (i - 1 >= B.size()) break; // Avoid accessing beyond precomputed Bernoulli numbers
+        
+        // Calculate the ith term of the series
+        double term = (B[i - 1] * pow(-4, i) * (1 - pow(4, i)) * pow(x, 2 * i - 1)) / std::tgamma(2 * i + 1);
+        sum += term;
+    }
+
+    return sum;
 }
